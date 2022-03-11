1452e374662aaaade387f2a8098518559b104699
Otávio Pessoal <otavio.basiliosaraiva@gmail.com>
2022-03-11 19:28:10 -0300

    Implementando o Swagger

--- src/main/java/br/com/rifa/email/controllers/EmailController.java
+++ src/main/java/br/com/rifa/email/controllers/EmailController.java
@@ -3,6 +3,8 @@ package br.com.rifa.email.controllers;
 import br.com.rifa.email.dtos.EmailDto;
 import br.com.rifa.email.models.EmailModel;
 import br.com.rifa.email.services.EmailService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@Api(value = "API REST envio de e-mails")
 public class EmailController {
 
     @Autowired
     EmailService emailService;
 
     @PostMapping("/sending-email")
+    @ApiOperation(value="Retorna o JSON enviado com o status SENT ou ERROR")
     public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
         EmailModel emailModel = new EmailModel();
         BeanUtils.copyProperties(emailDto, emailModel);
