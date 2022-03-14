caedaf59db5b36994089dbbcf8a5052128a63576
Otávio Pessoal <otavio.basiliosaraiva@gmail.com>
2022-03-14 14:48:54 -0300

    Implementando o RabbitMQ

---
+++ src/main/java/br/com/rifa/email/consumers/EmailConsumer.java
@@ -0,0 +1,25 @@
+package br.com.rifa.email.consumers;
+
+import br.com.rifa.email.dtos.EmailDto;
+import br.com.rifa.email.models.EmailModel;
+import br.com.rifa.email.services.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        System.out.println("Email Status: " + emailModel.getStatusEmail().toString());
+    }
+}
